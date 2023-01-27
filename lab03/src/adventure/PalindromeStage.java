a239572e987e66b70f3e3e26b3dcdf891c3e20af
Noah Adhikari <noahadhikari@berkeley.edu>
2023-01-27 10:55:07 -0800

    new tests

--- lab03/src/adventure/PalindromeStage.java
+++ lab03/src/adventure/PalindromeStage.java
@@ -40,13 +40,8 @@ public class PalindromeStage implements AdventureStage {
             }
 
             IntList numLst = digitsToIntList(input);
-            IntList reversedLst = null;
-            // Uncomment the following line if you are working through the optional section
-            // reversedLst = reverseList(numLst);
+            IntList reversedLst = reverseList(numLst);
 
-            if (reversedLst == null) {
-                break;
-            }
             if (numLst.equals(reversedLst)) {
                 System.out.println("Wow, nice room number!");
                 break;
