dea4f8d26913ce17b1259e59d1e8a459fca11cd4
Noah Adhikari <noahadhikari@berkeley.edu>
2023-02-26 13:00:36 -0800

    slight modifications to Map61B

--- lab07/src/Map61B.java
+++ lab07/src/Map61B.java
@@ -1,26 +1,30 @@
 import java.util.Set;
 
 /* Your implementation BSTMap should implement this interface. To do so,
- * append "implements Map61B<K,V>" to the end of your "public class..."
- * declaration, though you can use other formal type parameters if you'd like.
+ * append "implements Map61B<K, V>" to the end of your "public class..."
+ * declaration, though you can and should use other type parameters when
+ * necessary.
  */
 public interface Map61B<K, V> extends Iterable<K> {
 
-    /** Removes every mapping from this map. */
-    void clear();
+    /** Associates the specified value with the specified key in this map. 
+     *  If the map already contains the specified key, replaces the key's mapping
+     *  with the value specified. */
+    void put(K key, V value);
+
+    /** Returns the value to which the specified key is mapped, or null if this
+     * map contains no mapping for the key. */
+    V get(K key);
 
     /** Returns whether this map contains a mapping for the specified key. */
     boolean containsKey(K key);
 
-    /** Returns the value to which the specified key is mapped, or null if this
-      * map contains no mapping for the key. */
-    V get(K key);
 
     /** Returns the number of key-value mappings in this map. */
     int size();
-
-    /** Associates the specified value with the specified key in this map. */
-    void put(K key, V value);
+    
+    /** Removes every mapping from this map. */
+    void clear();
 
     /** Returns a Set view of the keys contained in this map. Not required for Lab 7.
       * If you don't implement this, throw an UnsupportedOperationException. */
