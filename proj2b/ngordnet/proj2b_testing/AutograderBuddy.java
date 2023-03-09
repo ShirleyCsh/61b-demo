831edd5ab313249019ffca123f08dd4d33684fd5
Shirley Chen <shirleyc815@berkeley.edu>
2023-03-09 10:16:56 -0800

    autograder buddy

--- proj2b/ngordnet/proj2b_testing/AutograderBuddy.java
+++ proj2b/ngordnet/proj2b_testing/AutograderBuddy.java
@@ -1,14 +1,17 @@
 package ngordnet.proj2b_testing;
 
 import ngordnet.browser.NgordnetQueryHandler;
-
+import ngordnet.main.HyponymsHandler;
+import ngordnet.ngrams.NGramMapWithPopularity;
+import ngordnet.wordnet.WordNet;
 
 public class AutograderBuddy {
-    /** Returns a HyponymHandler */
     public static NgordnetQueryHandler getHyponymHandler(
             String wordFile, String countFile,
             String synsetFile, String hyponymFile) {
 
-        return null;
+        NGramMapWithPopularity ngm = new NGramMapWithPopularity(wordFile, countFile);
+        WordNet wn = new WordNet(synsetFile, hyponymFile);
+        return new HyponymsHandler(ngm, wn);
     }
 }
